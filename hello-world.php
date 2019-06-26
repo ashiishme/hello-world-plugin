d53dea98b2bce311017f989d14469317577e72f2
Ashish <iloveh4ck1ng@gmail.com>
2019-06-26 19:46:59 +0545

    Hello World

---
+++ hello-world.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * @package hello-world
+ *
+ * Plugin Name: Hello World
+ * Plugin URI: https://www.ashiish.me/
+ * Description: A plugin to greet the visitors or just another test plugin to be honest.
+ * Version: 1.0.0
+ * Author: Ashish Yadav
+ * Author URI: https://www.ashiish.me/
+ * License: GPLv2 or later
+ * Text Domain: hello-world
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ */
+
